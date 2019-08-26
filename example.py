77049b99c023b9fcc676ec685809f7602266489a
ady95 <adyz95@gmail.com>
2019-08-26 22:37:54 +0900

    MODIFY 오류 수정

--- example.py
+++ example.py
@@ -6,7 +6,8 @@ from dejavu import Dejavu
 from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
 
 # load config from a JSON file (or anything outputting a python dictionary)
-with open("dejavu.cnf.SAMPLE") as f:
+# with open("dejavu.cnf.SAMPLE") as f:
+with open("dejavu.cnf.SQLITE_SAMPLE") as f:
     config = json.load(f)
 
 if __name__ == '__main__':
